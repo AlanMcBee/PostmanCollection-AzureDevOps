49d14738d7010c27d4e2f0c39abaaea0041c322c
Alan McBee <alan.mcbee@microsoft.com>
2021-04-01 19:05:08 -0700

    Failing effort to use sample code

--- VstsOpenApi-to-PostmanCollection/app.ts
+++ VstsOpenApi-to-PostmanCollection/app.ts
@@ -1,3 +1,18 @@
-var Converter = require('openapi-to-postmanv2');
+let Converter = require('openapi-to-postmanv2'),
+    FS = require('fs')
+    ;
 
 console.log('VstsOpenApi-to-PostmanCollection');
+
+let openApiJson = FS.readFileSync('..\\vsts-rest-api-specs\\specification\\core\\6.1\\core.json', {encoding: 'UTF8'})
+
+Converter.convert({ type: 'string', data: openApiJson },
+  {}, (_err, conversionResult) => {
+    if (!conversionResult.result) {
+      console.log('Could not convert', conversionResult.reason);
+    }
+    else {
+      console.log('The collection object is: ', conversionResult.output[0].data);
+    }
+  }
+);
