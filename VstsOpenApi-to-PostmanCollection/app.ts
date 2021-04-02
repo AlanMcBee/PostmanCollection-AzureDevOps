94c6add6e82901d82be4a545976874f969945721
Alan McBee <alan.mcbee@microsoft.com>
2021-04-02 10:41:55 -0700

    Attempting (failing) to use a Promise here

--- VstsOpenApi-to-PostmanCollection/app.ts
+++ VstsOpenApi-to-PostmanCollection/app.ts
@@ -1,13 +1,24 @@
 let Converter = require('openapi-to-postmanv2');
-let FS = require('node:fs')
+let FS = require('fs')
+
+export { };
 
 console.log('VstsOpenApi-to-PostmanCollection');
 
-let openApiJson = FS.readFileSync('vsts-rest-api-specs\\specification\\core\\6.1\\core.json');
+let openApiJson = await FS.readFile('vsts-rest-api-specs\\specification\\core\\6.1\\core.json', { 'encoding': 'utf8' });
+
+// Remove the UTF-8 BOM because engineers need stupid problems like this to worry about. I really don't enjoy working with Javascript. No, I don.t. Why isn't this just a new property in the options?
+openApiJson = openApiJson.replace(/^\uFEFF/, '');
 
 let openApi = JSON.parse(openApiJson);
 
+// console.log('2: ' + JSON.stringify(openApi));
+
 openApi['openapi'] = "3.0";
+
+// console.log('3: ' + JSON.stringify(openApiJson));
+
+let collection = null;
 
 Converter.convert(
     { type: 'json', data: openApi },
@@ -17,7 +28,10 @@ Converter.convert(
             console.log('Could not convert\n', conversionResult.reason);
         }
         else {
-            console.log('The collection object is: ', conversionResult.output[0].data);
+            collection = conversionResult.output[0].data;
         }
     }
 );
+
+console.log(collection);
+
